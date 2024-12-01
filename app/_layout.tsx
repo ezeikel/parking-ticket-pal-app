b71c20d8ba37d1a2aa019a4420e5fd590e772ccd
Ezeikel Pemberton <hi@ezeikel.dev>
2024-12-01 00:45:42 +0000

    chore: sentry profiling.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,29 @@
-import { Slot } from 'expo-router';
-import Providers from "@/providers";
+import { useEffect } from 'react';
+import { Slot, useNavigationContainerRef } from 'expo-router';
+import Constants, { ExecutionEnvironment } from "expo-constants";
 import * as Sentry from '@sentry/react-native';
+import Providers from "@/providers";
+
+const navigationIntegration = Sentry.reactNavigationIntegration({
+  enableTimeToInitialDisplay: Constants.executionEnvironment === ExecutionEnvironment.StoreClient,
+});
 
 Sentry.init({
   dsn: 'https://e7a94f57d66133d60cf00d10a65df1d1@o358156.ingest.us.sentry.io/4508390020415488',
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // enableSpotlight: __DEV__,
+  tracesSampleRate: 1.0,
+  integrations: [navigationIntegration],
+  enableNativeFramesTracking: Constants.executionEnvironment === ExecutionEnvironment.StoreClient,
 });
 
 const RootLayout = () => {
+  const ref = useNavigationContainerRef();
+
+  useEffect(() => {
+    if (ref) {
+      navigationIntegration.registerNavigationContainer(ref);
+    }
+  }, [ref]);
+
   return (
     <Providers>
       <Slot />
