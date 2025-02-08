73dadcb7674943f4197c9aeaaeee03dcb892a256
Ezeikel Pemberton <hi@ezeikel.dev>
2025-02-08 16:42:52 +0000

    chore: using pnpm instead of bun. Upgraded all deps

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,8 +10,12 @@ const navigationIntegration = Sentry.reactNavigationIntegration({
 
 Sentry.init({
   dsn: 'https://e7a94f57d66133d60cf00d10a65df1d1@o358156.ingest.us.sentry.io/4508390020415488',
+  _experiments: {
+    replaysSessionSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
+  },
   tracesSampleRate: 1.0,
-  integrations: [navigationIntegration],
+  integrations: [navigationIntegration, Sentry.mobileReplayIntegration()],
   enableNativeFramesTracking: Constants.executionEnvironment === ExecutionEnvironment.StoreClient,
 });
 
